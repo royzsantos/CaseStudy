822eecaf55f0621d87350a30d876d8bd19f4905d
RoySantos <RoySantos@ISD-PF0ZKD2N.ph.ibm.com>
2019-06-27 00:46:48 +0800

    c

--- src/main/java/com/casestudy_2/Main.java
+++ src/main/java/com/casestudy_2/Main.java
@@ -4,7 +4,9 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello World!");
+		MessageUtil messageUtil = new MessageUtil("Hello World!!!");
+		
+		messageUtil.printMessage();
 	}
 
 }
